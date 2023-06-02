0d131879fa41a346876e54b6648667713ede080e
Andrei <andrei_zhigachev@bk.ru>
2023-06-02 18:49:11 +0300

    сделан базовый роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter, 
+  Routes, 
+  Route
+} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
